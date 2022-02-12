fab8a511c46e548a376916c4eed4b40099892ace
bogxoxl <BogdanXoxl>
2022-02-12 20:14:08 +0300

    finish practicum

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Checkbox, Divider, InputNumber, List} from 'antd';
+import {useState} from "react";
+import {Container, Header} from "./index.styles";
+import VirtualList from 'rc-virtual-list';
+import {CheckboxValueType} from "antd/es/checkbox/Group";
+import {CheckboxChangeEvent} from "antd/es/checkbox";
 
-function App() {
+const CheckboxGroup = Checkbox.Group;
+
+const plainOptions = ['0-9', 'a-z', 'A-Z'];
+const Alphabet = {
+    '0-9': "0123456789",
+    'a-z': "qwertyuiopasdfghjklmnbvcxz",
+    'A-Z': "qwertyuiopasdfghjklmnbvcxz".toUpperCase()
+};
+
+const App = () => {
+  const [checkedList, setCheckedList] = useState<CheckboxValueType[]>();
+  const [indeterminate, setIndeterminate] = useState<boolean>(false);
+  const [checkAll, setCheckAll] = useState<boolean>(false);
+  const [counter, setCounter] = useState<number>(10);
+
+  const [data, setData] = useState<string[]>([]);
+
+  const onChange = (list: CheckboxValueType[]) => {
+    setCheckedList(list);
+    setIndeterminate(!!list.length && list.length < plainOptions.length);
+    setCheckAll(list.length === plainOptions.length);
+  };
+
+  const onCheckAllChange = (e:CheckboxChangeEvent) => {
+    setCheckedList(e.target.checked ? plainOptions : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+  };
+
+
+  const onClick = () => {
+      if(counter > 0 && checkedList?.length){
+          let pas = "";
+          let A = "";
+          checkedList.forEach((el) => {
+              // @ts-ignore
+              A += Alphabet[el];
+          });
+          console.log(A);
+          for(let i=0; i < counter; i++){
+            pas +=  A.charAt(Math.floor(Math.random() * A.length));
+          }
+          setData(prev => [...prev, pas]);
+      }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+          <Divider>Генератор паролей</Divider>
+          <List
+              header={
+                  <Header>
+                      <div>
+                          <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+                              Все
+                          </Checkbox>
+                          <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
+                          <InputNumber size={"small"} min={0} max={100} value={counter} onChange={(value) => setCounter(value)}/>
+                      </div>
+                      <Button type="primary" onClick={onClick} block>Сгенерировать</Button>
+                  </Header>
+              }
+              bordered
+          >
+              <VirtualList data={data} height={320} itemHeight={30} itemKey="id">
+                  {item => (
+                      <List.Item>
+                          {item}
+                      </List.Item>
+                  )}
+              </VirtualList>
+          </List>
+      </Container>
   );
-}
+};
 
 export default App;
+
---
+++ src/index.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+  max-width: 380px;
+  margin: 0 auto;
+  padding-top: 12vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+`;
